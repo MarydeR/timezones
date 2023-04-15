1af056220e5979cde5689de5dabe10fbc71dac5e
MarydeR <122215523+MarydeR@users.noreply.github.com>
2023-04-15 20:51:46 +0200

    timezones update

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ let honolulutime = moment()
 
 let honolulu = document.querySelector("#current-date");
 honolulu.innerHTML = `It is ${honolulutime} in Honolulu, Hawaii`;
+
+let paristenyrs = moment()
+  .tz("Europe/Paris")
+  .add(10, "year")
+  .format("dddd, MMMMM D, YYYY hh:mm:ss a");
+
+let paris = document.querySelector("#future-date");
+paris.innerHTML = `In 10 years from now, it will be ${paristenyrs} in Paris, France`;
+
+let localtz = moment.tz.guess();
+let localzone = document.querySelector("#local-time-zone");
+localzone.innerHTML = localtz;
